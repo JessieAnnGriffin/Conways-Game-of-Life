cd58cddcaf6799a035f59c4503bca62a4f879336
Jessie <jessie.griffin@icloud.com>
2020-08-18 22:17:03 -0400

    Project Created - Some folders created

---
+++ ConwaysGameOfLife/ConwaysGameOfLife/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConwaysGameOfLife
+//
+//  Created by Jessie Ann Griffin on 8/18/20.
+//  Copyright © 2020 Jessie Griffin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
